384f0e3277d864249e314990d3fd9bf2d1512928
weimingdan <1508281800@qq.com>
2019-09-10 10:58:10 +0800

    add div function

--- TestGitSvn/TestGitSvn.cpp
+++ TestGitSvn/TestGitSvn.cpp
@@ -19,6 +19,15 @@ int multi(int a, int b)
 	return a*b;
 }
 
+int divNumber(int a, int b)
+{
+	if (b == 0)
+	{
+		return 0;
+	}
+	return a / b;
+}
+
 int main()
 {
 	std::cout << "hello world" << std::endl;
