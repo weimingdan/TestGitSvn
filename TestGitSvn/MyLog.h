ea7cd186c631e8edd49fcf79d3abf63a9c2424c5
weimingdan <1508281800@qq.com>
2019-09-18 13:46:42 +0800

    add function,line,file info to log

--- TestGitSvn/MyLog.h
+++ TestGitSvn/MyLog.h
@@ -13,9 +13,14 @@ private:
 	static std::shared_ptr<spdlog::logger> s_CoreLogger;
 	static std::shared_ptr<spdlog::logger> s_ClientLogger;
 };
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define DETAILLOG(...) "[{}:#{}] {}: {}", __FILENAME__ , __LINE__, __FUNCTION__, fmt::format(__VA_ARGS__)
 
-#define CORE_TRACE(...)    ::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define CORE_INFO(...)     ::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CORE_INFOD(...)     ::Log::GetCoreLogger()->info(DETAILLOG(__VA_ARGS__))
 #define CORE_WARN(...)     ::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CORE_WARND(...)     ::Log::GetCoreLogger()->warn(DETAILLOG(__VA_ARGS__))
 #define CORE_ERROR(...)    ::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CORE_FATAL(...)    ::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define CORE_ERRORD(...)    ::Log::GetCoreLogger()->error(DETAILLOG(__VA_ARGS__))
+#define CORE_FATAL(...)    ::Log::GetCoreLogger()->critical(DETAILLOG(__VA_ARGS__))
+
